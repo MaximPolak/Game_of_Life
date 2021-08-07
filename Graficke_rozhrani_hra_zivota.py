fc7cfb12d52397e20964b6ba211470e29126aadd
Maxim Polák <polakmaxi@gmail.com>
2021-08-07 17:14:39 +0200

    Upravení kódu dle připomínek

--- Graficke_rozhrani_hra_zivota.py
+++ Graficke_rozhrani_hra_zivota.py
@@ -6,27 +6,27 @@ vyska = 20
 sirka = 30
 hra = Hra_zivota.HraZivota(sirka, vyska)
 
-hraci_plocha = [[sg.Button("", key=(radek, sloupec), size=(2, 1), pad=(0, 0), button_color=("blue", "#0000FF")) for sloupec in range(sirka)] for radek in range(vyska)]
-zbytek_plochy = [[sg.Button("Start", size=(6, 2))]]
+hraci_plocha = [[sg.Button(
+    "", key=(radek, sloupec), size=(2, 1), pad=(0, 0), button_color=("blue", "#4964A9")) 
+        for sloupec in range(sirka)] for radek in range(vyska)]
+zbytek_plochy = [[sg.Button("Další kolo", size=(8, 2))]]
 rozmisteni = hraci_plocha + zbytek_plochy
 
 okno = sg.Window("Hra života", rozmisteni)
+
+barvy = {True: ("red", "#E98478"), False: ("blue", "#4964A9")}
 
 while True:
     udalost, hodnoty = okno.read()
     if udalost == sg.WINDOW_CLOSED:
         break
-    elif udalost and udalost != "Start":
-        okno[udalost].update(button_color=("red", "#FF0000"))
-        hra.zmen_pole(udalost)
-    elif udalost == "Start":
-        while True:
-            for stav, indexy in hra.policka_na_update():
-                if stav == True:
-                    okno[indexy].update(button_color=("red", "#FF0000"))
-                else:
-                    okno[indexy].update(button_color=("blue", "#0000FF"))
-            hra.update()
-            time.sleep(1)
+    elif udalost != "Další kolo":
+        barva = hra.zmen_pole(udalost)
+        okno[udalost].update(button_color=(barvy[barva]))
         
-okno.close()   
+    elif udalost == "Další kolo":
+        for stav, indexy in hra.policka_na_update():
+            okno[indexy].update(button_color=barvy[stav])
+        hra.update()
+        
+okno.close()                                                                          
